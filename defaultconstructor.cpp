989a7bcb3360d0a3a9bf74ec817938a91912cd4e
ishitamehta663 <154406479+ishitamehta663@users.noreply.github.com>
2024-04-25 19:48:03 +0530

    Add files via upload

---
+++ defaultconstructor.cpp
@@ -0,0 +1,21 @@
+// C++ program to demonstrate the use of default constructor
+
+#include <iostream>
+using namespace std;
+
+class  Wall {
+  private:
+    double length;
+
+  public:
+    Wall() {
+      length = 5.5;
+      cout << "Creating a wall." << endl;
+      cout << "Length = " << length << endl;
+    }
+};
+
+int main() {
+  Wall wall1;
+  return 0;
+}

7e440f41a7fcabec102b57950b9d45b1c1ec85d2
ishitamehta663 <154406479+ishitamehta663@users.noreply.github.com>
2024-04-27 13:44:06 +0530

    Add files via upload

---
+++ passbyvalue.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void swap(int a, int b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+int main()
+{
+	int x = 5;
+	int y = 10;
+
+	cout << "Before Swapping:\n";
+	cout << "x = " << x << ", y = " << y << endl;
+
+	swap(x, y);
+
+	cout << "After Swapping:\n";
+	cout << "x = " << x << ", y = " << y << endl;
+
+	return 0;
+}
